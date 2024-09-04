49f591abb9f7753ed696a5f82326cb6ff99baff0
Darya <102173317+KokonovaDS@users.noreply.github.com>
2024-09-04 20:36:42 +0400

    Update task4.py

--- task4/task4.py
+++ task4/task4.py
@@ -1,1 +1,31 @@
-print('task4')
+import argparse
+
+parser = argparse.ArgumentParser(description='task4, numbers')
+parser.add_argument('numbers', help='массив целых чисел', type=argparse.FileType('r', encoding='UTF-8'))
+
+args = parser.parse_args()
+
+numbers = []
+step = 0
+
+for item in args.numbers.readlines():
+    if '\n' in item:
+        numbers.append(int(item[:-1]))
+    else:
+        numbers.append(int(item))
+result = sum(numbers) // len(numbers)
+
+while True:
+    for number in numbers:
+        if number < result:
+            while number != result:
+                number += 1
+                step += 1
+        elif number > result:
+            while number != result:
+                number -= 1
+                step += 1
+    else:
+        break
+
+print(step)
