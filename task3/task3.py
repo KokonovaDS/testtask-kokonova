3c38aad32793bba06fd887819d92c89a1ce2624d
Darya <102173317+KokonovaDS@users.noreply.github.com>
2024-09-04 23:07:49 +0400

    Update task3.py

--- task3/task3.py
+++ task3/task3.py
@@ -17,7 +17,7 @@ def iterate_nested_list(nested_values_list, test_id, test_value):
 parser = argparse.ArgumentParser(description='task3, forming json')
 parser.add_argument('tests', help='путь до tests.json', type=argparse.FileType('r', encoding='UTF-8'))
 parser.add_argument('values', help='путь до values.json', type=argparse.FileType('r', encoding='UTF-8'))
-parser.add_argument('report', help='путь до report.json', type=argparse.FileType('w', encoding='UTF-8'))
+parser.add_argument('report', help='путь до report.json', type=argparse.FileType('r', encoding='UTF-8'))
 
 args = parser.parse_args()
 
@@ -25,7 +25,9 @@ tests_dict = json.load(args.tests)
 values_dict = json.load(args.values)
 report_dict = tests_dict
 result = {}
+args.report.close()
+report = open(args.report.name, "w")
 
 for values_list in values_dict['values']:
     result = iterate_nested_list(tests_dict['tests'], values_list['id'], values_list['value'])
-json.dump(result, args.report)
+json.dump(result, report)
